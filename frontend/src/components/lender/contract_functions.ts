0d4a798dc1b1705735fc7166fb545d18d79b7eb8
Bchara  Abou Jaffal <bchara.aboujaffal@gtslb.com>
2024-09-24 16:21:41 +0300

    connected postloan function

--- frontend/src/components/lender/contract_functions.ts
+++ frontend/src/components/lender/contract_functions.ts
@@ -1,22 +1,25 @@
 import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
 
-const config = new AptosConfig({ network: Network.TESTNET });
+const config = new AptosConfig({ network: Network.DEVNET });
 const aptos = new Aptos(config);
 
-const prefix = `${process.env.ADDRESS}::${process.env.MODULE}::`;
-const contract_address = process.env.CONTRACT_ADDRESS;
+const prefix = `${process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}::${process.env.NEXT_PUBLIC_MAIN_MODULE}::`;
+const contract_address = "process.env.NEXT_PUBLIC_CONTRACT_ADDRESS";
 
-const postLoan = async(amount: number, interest: number, time: number, caller_address: string, ) => {
+const postLoan = async(amount: number, interest: number, time: number, caller_address: string) => {
     const functionName = "postLoan"; 
     const fullFunctionName = `${prefix}${functionName}` as `${string}::${string}::${string}`; 
 
-    await aptos.transaction.build.simple({
+    const transaction = await aptos.transaction.build.simple({
         sender: caller_address,
         data: {
           function: fullFunctionName,
           functionArguments: [contract_address, amount, interest, time],
         },
       });
+
+      return transaction;
+
 };
 
 const liquidateLoan = async(loan_id: number, caller_address: string) => {
